8b23f2b69bb82e92f0b1679cb2589247d305e025
Semyon Chshemelinin <TwiQSam@yandex.kz>
2024-08-18 16:55:12 +0500

    add allure report and more

---
+++ test_data.py
@@ -0,0 +1,5 @@
+BASE_URL = "https://qa-scooter.praktikum-services.ru/api/v1"
+
+COURIER_CREATION_URL = f"{BASE_URL}/courier" #Создание курьера
+COURIER_LOGIN_URL = f"{BASE_URL}/courier/login" #Логин курьера
+ORDER_CREATION_AND_LIST_URL = f"{BASE_URL}/orders" #Создание заказа и Список заказов
